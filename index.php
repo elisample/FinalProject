afaa3150bfd0469a6863c013ac4f39060ac22128
elisample <103210286+elisample@users.noreply.github.com>
2022-11-17 11:49:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,32 +6,11 @@
  * Description: Final Project- One Stop Sausage. index page
  */
 
+//Load application settings
+require_once ('application/config.php');
 
-require 'vendor/autoload.php';
+//Load the autoloader
+require_once ('vendor/autoload.php');
 
-
-$sausage_controller = new SausageController();
-
-$action = "all";
-
-if(isset($_GET['action'])){
-    $action = $_GET['action'];
-}
-
-//display all the toys
-if($action == "all"){
-    $sausage_controller->all();
-}
-//display an error
-else if($action == "error"){
-    $message ="we are sorry, but an error has occurred";
-    if(isset($_GET['message'])){
-        $message = $_GET['message'];
-    }
-    $sausage_controller->error($message);
-}
-
-else{
-    $message = "Invalid action was requested";
-    $sausage_controller->error($message);
-}
+//Load the dispatcher class
+new Dispatcher();
